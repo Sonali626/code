c231a44368cfe1235ab36fc3cd89046d1f805333
Sonali626 <sonali1618priya@gmail.com>
2025-02-25 22:08:09 +0530

    Add files via upload

---
+++ factorialusingrecursion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int fact(int n);
+int main()
+{
+    int x;
+    printf("Enter a number which you want to find factorial:");
+    scanf("%d",&x);
+    int c =fact(x);
+    printf("Factoeial of %d is %d\n",x,c);
+    return 0;
+}
+int fact(int n)
+{
+    if(n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*fact(n-1);
+    }
+}
