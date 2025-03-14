4affb5d0ab4dd75ab1d835bbf2c7d28f3778b9d6
Sonali626 <sonali1618priya@gmail.com>
2025-03-14 09:56:45 +0530

    Add files via upload

---
+++ Explicit_type_casting.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    double a=9.45,b=4.55;
+    //explicitly defined by user
+    int sum=(int)a+(int)b;
+    printf("Addition of a and b is %d",sum);
+    return 0;
+}
