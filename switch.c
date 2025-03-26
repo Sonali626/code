69e0380ffa240c3f89ee2d5f274d8d4cee154cf7
Sonali Singh <sonali1618priya@gmail.com>
2025-03-26 23:22:23 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int day;
+    printf("Enter any day 1-7 :");
+    scanf("%d",&day);
+    switch(day)
+   {
+     case 1:
+      printf("Day is Sunday");
+      break;
+    case 2:
+      printf("Day is Monday");
+      break;
+    case 3:
+      printf("Day is Thuesday");
+      break;
+    case 4:
+      printf("Day is Wednesday");
+      break;
+    case 5:
+      printf("Day is Thursday");
+      break;
+    case 6:
+      printf("Day is Friday");
+      break;
+    case 7:
+      printf("Day is Saturday");
+      break;
+    default:
+      printf("wrong input");
+    }
+    return 0;
+}
